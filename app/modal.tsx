aad50c3bbfccdb8269833cf3bd8d701cbf351441
kishu279 <souravpoddar93042@gmail.com>
2025-11-22 01:51:36 +0530

    added the landing page

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,15 +1,31 @@
-import { Link } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Link } from "expo-router";
+import { StyleSheet, View } from "react-native";
 
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
 
 export default function ModalScreen() {
   return (
     <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
+      <View
+        style={{
+          flexDirection: "column",
+          alignItems: "center",
+          padding: 18,
+        }}
+      >
+        <ThemedText type="title" style={styles.title}>
+          Notify
+        </ThemedText>
+        <ThemedText type="subtitle" style={styles.para}>
+          A Note Taking Application
+        </ThemedText>
+      </View>
+
       <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
+        <ThemedText type="link" style={styles.para}>
+          Let's go
+        </ThemedText>
       </Link>
     </ThemedView>
   );
@@ -18,12 +34,23 @@ export default function ModalScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20,
   },
   link: {
     marginTop: 15,
     paddingVertical: 15,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    // borderColor: "black",
+    borderWidth: 2,
+    padding: 6,
+  },
+  para: {
+    textAlign: "center",
+    fontSize: 16,
   },
 });
